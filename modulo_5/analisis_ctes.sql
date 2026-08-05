d35f96902935c2fbc9e314c07e87cb11264e1d72
facundoveron <veronariel98@hotmail.com>
2026-08-05 14:35:08 -0300

    actualizando

--- modulo_5/analisis_ctes.sql
+++ modulo_5/analisis_ctes.sql
@@ -1,6 +1,9 @@
 --- Asegúrate de estar conectado a tu base de datos de PostgreSQL donde tienes las tablas de ventas, 
 --- regiones y productos (creadas en módulos anteriores).
 
+--- 1. Definición de la CTE (Common Table Expression):
+--- Se unen las tablas ventas, regiones y productos para calcular el 
+--- monto total de ventas por cada región.
 WITH ventas_por_region AS (
     SELECT
         r.nombre,
@@ -10,6 +13,10 @@ WITH ventas_por_region AS (
     JOIN productos p ON v.id_producto = p.id_producto
     GROUP BY r.nombre
 )
+
+--- 2. Consulta principal y filtrado:
+--- Se obtienen las regiones desde la CTE y se aplica un filtro mediante una subconsulta
+--- para mostrar únicamente aquellas regiones cuyas ventas superen el promedio general.
 SELECT
     nombre,
     total_ventas
